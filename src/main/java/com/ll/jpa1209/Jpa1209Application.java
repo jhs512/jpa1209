bd3d3d884f719140e226e729b6263fc680bb9f88
naraekwon86 <wde0530@gmail.com>
2023-12-09 08:20:20 +0900

    setting

---
+++ src/main/java/com/ll/jpa1209/Jpa1209Application.java
@@ -0,0 +1,13 @@
+package com.ll.jpa1209;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Jpa1209Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Jpa1209Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/ll/jpa1209/Jpa1209ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.jpa1209;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jpa1209ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
